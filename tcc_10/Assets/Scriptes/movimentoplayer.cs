4883cbd6a18b85fffd870b9f3131c564e0f2a315
Isah <125080557+IsadoraMillena@users.noreply.github.com>
2023-05-03 11:23:48 -0300

    cria mecanicas

--- tcc_10/Assets/Scriptes/movimentoplayer.cs
+++ tcc_10/Assets/Scriptes/movimentoplayer.cs
@@ -7,6 +7,7 @@ public class movimentoplayer : MonoBehaviour
     public float speed = 5f;
     public float jumpForce;
     public GameObject projetil;
+    public GameObject bombaM;
     public Transform posicaoProjetil;
     private Rigidbody2D rb;
     public bool isGrounded;
@@ -14,17 +15,31 @@ public class movimentoplayer : MonoBehaviour
     public LayerMask layerGround;
     public int moedas;
     public TextMeshProUGUI textoMoedas;
+    private int maca;
+    public TextMeshProUGUI textomaca;
+    private int agua;
+    public TextMeshProUGUI textoagua;
+    
+    private inimigoR inimigoR;
 
 
     public GameObject inimigo;
 
     public float forcaimpulso;
-    
+
+
+    public GameObject historia;
 
 
     void Start()
     {
+
+        inimigoR = FindObjectOfType(typeof(inimigoR)) as inimigoR;
+
+        inimigoR.speed = 0;
         rb = GetComponent<Rigidbody2D>();
+
+        StartCoroutine(tempohistoria());
         
     }
     void Update()
@@ -44,7 +59,7 @@ public class movimentoplayer : MonoBehaviour
             Debug.Log("pulo");
         }
 
-        atirar();
+        //atirar();
     }
     private void FixedUpdate()
     {
@@ -73,6 +88,59 @@ public class movimentoplayer : MonoBehaviour
             Destroy(inimigo.gameObject);
             rb.AddForce(Vector2.up * forcaimpulso, ForceMode2D.Impulse);
         }
+
+        if (col.gameObject.tag == "maça")
+        {
+            maca += 1;
+
+            textomaca.text = maca.ToString();
+
+            Destroy(col.gameObject);
+        }
+
+        if (col.gameObject.tag == "agua")
+        {
+            agua += 1;
+
+            textoagua.text = agua.ToString();
+
+            Destroy(col.gameObject);
+        }
+
+    }
+
+    public void tiro()
+    {
+        Instantiate(projetil, posicaoProjetil.position, Quaternion.identity);
+    }
+
+    public void bomba()
+    {
+        Instantiate(bombaM, posicaoProjetil.position, Quaternion.identity);
+    }
+
+    public void pulo()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    public void direita()
+    {
+
+    }
+
+    public void esquerda()
+    {
+
+    }
+
+    IEnumerator tempohistoria()
+    {
+        
+        yield return new WaitForSeconds(7);
+        historia.SetActive(false);
+        inimigoR.speed = 5;
+
     }
         #region 
     public void atirar()
